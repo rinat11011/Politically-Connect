eb213a737ba9fd767bbe50a97970c5541497a7d5
rinat.vinokurov <45692844+rinat11011@users.noreply.github.com>
2020-09-22 19:15:32 +0300

    Add files via upload

---
+++ src/test/java/com/politiclyconnect/partyregisty/PartyRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politiclyconnect.partyregisty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
